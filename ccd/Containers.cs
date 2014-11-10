910e093572d0c0356970f2c8691fbdd8feea488d
Elaias <ladakar@ya.ru>
2014-11-10 16:27:10 +0300

    add mechanics class and containers

---
+++ ccd/Containers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccd
+{
+    public class Player
+    {
+        public List<Card> Deck = new List<Card>();
+        public Card[] Hand = new Card[] {};
+        public int Hp;
+        public int Resource;
+    }
+
+    public class Card
+    {
+        public int Hp;
+        public int Atk;
+        public CardSpecType Type = 0;
+    }
+
+    public enum CardSpecType
+    {
+        Nothing = 0,
+        AtkPlayer = 1,
+        AtkAllCards = 2
+    }
+}
---
+++ ccd/GameMechanics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccd
+{
+    public class GameMechanics
+    {
+        private Player _player1;
+        private Player _player2;
+
+        public GameMechanics()
+        {
+            _player1.Deck = _createNewDeck();
+            _player2.Deck = _createNewDeck();
+        }
+
+        private List<Card> _createNewDeck()
+        {
+            List<Card> newDeck = new List<Card>();
+            newDeck.Add(new Card() {Atk = 1, Hp = 2});
+            newDeck.Add(new Card() {Atk = 2, Hp = 1});
+
+            return newDeck;
+        }
+    }
+}
